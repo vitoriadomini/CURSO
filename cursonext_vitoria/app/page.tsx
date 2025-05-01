177221ecc382a3702f4d931d2eb6a92ab1c747c5
vitoriadomini <dominis.ads@gmail.com>
2025-05-01 21:56:45 +0000

    git para mostrar para o Danilo

---
+++ cursonext_vitoria/app/page.tsx
@@ -0,0 +1,86 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1>O Menestrel – William Shakespeare</h1>
+        <p>
+Depois de algum tempo você aprende a diferença,
+a sutil diferença entre dar a mão e acorrentar uma alma.
+E você aprende que amar não significa apoiar-se.
+E que companhia nem sempre significa segurança.
+Começa a aprender que beijos não são contratos e que presentes não são promessas.
+Começa a aceitar suas derrotas com a cabeça erguida e olhos adiante,
+com a graça de um adulto e não com a tristeza de uma criança.
+Aprende a construir todas as suas estradas no hoje,
+porque o terreno do amanhã é incerto demais para os planos,
+e o futuro tem o costume de cair em meio ao vão.
+Depois de um tempo você aprende que o sol queima se ficar exposto por muito tempo.
+E aprende que, não importa o quanto você se importe,
+algumas pessoas simplesmente não se importam…
+E aceita que não importa quão boa seja uma pessoa,
+ela vai feri-lo de vez em quando e você precisa perdoá-la por isso.
+Aprende que falar pode aliviar dores emocionais.
+Descobre que se leva anos para construir confiança e apenas segundos para destruí-la…
+E que você pode fazer coisas em um instante das quais se arrependerá pelo resto da vida.
+Aprende que verdadeiras amizades continuam a crescer mesmo a longas distâncias.
+E o que importa não é o que você tem na vida,
+mas quem você tem na vida.
+E que bons amigos são a família que nos permitiram escolher.
+Aprende que não temos de mudar de amigos se compreendemos que os amigos mudam…
+Percebe que seu melhor amigo e você podem fazer qualquer coisa,
+ou nada, e terem bons momentos juntos.
+Descobre que as pessoas com quem você mais se importa na vida são tomadas de você muito depressa…
+por isso sempre devemos deixar as pessoas que amamos com palavras amorosas;
+pode ser a última vez que as vejamos.
+Aprende que as circunstâncias e os ambientes têm influência sobre nós,
+mas nós somos responsáveis por nós mesmos.
+Começa a aprender que não se deve comparar com os outros,
+mas com o melhor que pode ser.
+Descobre que se leva muito tempo para se tornar a pessoa que quer ser,
+e que o tempo é curto.
+Aprende que não importa onde já chegou, mas para onde está indo…
+mas, se você não sabe paraonde está indo, qualquer caminho serve.
+Aprende que,ou você controla seus atos, ou eles o controlarão…
+e que ser flexível nãosignifica ser fraco,ou não ter personalidade,
+pois não importa quão delicada e frágil seja uma situação,
+sempre existem, pelomenos, dois lados.
+Aprende que heróis são pessoas que fizeram o que era necessário fazer,
+enfrentando as conseqüências.
+Aprende que paciência requer muita prática.
+Descobre que algumas vezes a pessoa que você espera que o chute quandoocê cai
+é uma das poucas queo ajudam a levantar-se.
+Aprende que maturidade tem mais a ver com os tipos de experiência que se teve
+e o que você aprendeu com elas do que com quantos aniversários você celebrou.
+Aprende que há mais dos seus pais em você do que você supunha.
+Aprende que nunca sedeve dizer a uma criança que sonhos são bobagens…
+Poucas coisas são tão humilhantes e seria uma tragédia se ela acreditasse nisso.
+Aprende que quando está com raiva temo direito de estar com raiva,
+mas isso não te dá o direito de ser cruel.
+Descobre que só porque alguém não o ama do jeito que você quer que ame
+não significa que esse alguém não o ama com tudo o que pode,
+pois existem pessoas que nos amam,
+mas simplesmente nãosabem como demonstrar ou viver isso.
+Aprende que nem sempre é suficiente ser perdoado por alguém…
+Algumas vezes você temde aprender a perdoar a si mesmo.
+Aprende que com a mesma severidade com que julga,
+você será em algum momento condenado.
+Aprende que não importa em quantos pedaços seu coração foi partido,
+o mundo não pára para que você o conserte.
+Aprende que o tempo não é algo que possa voltar.
+Portanto, plante seu jardim e decore sua alma,
+em vez de esperar que alguém lhe traga flores.
+E você aprende que realmente pode suportar…
+que realmente é forte,
+e que pode ir muito mais longe depois de pensar que não se pode mais.
+E que realmente a vida tem valor e que você tem valor diante da vida!
+Nossas dúvidas são traidoras e nos fazem perder o bem
+que poderíamos conquistar se não fosse o medo de tentar.</p>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        
+      </footer>
+    </div>
+  );
+}
